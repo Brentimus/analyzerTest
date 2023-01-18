bc277f7e449407b4feb12f9b6759b78d343327d5
Brentimus <Gamepopins@gmail.com>
2023-01-18 18:33:12 +1000

    Add Simple Parser

---
+++ SimpleParser/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using SimpleParser = SimpleParser.SimpleParser;
+
+internal class program
+{
+    private static void Main(string[] args)
+    {
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+        using (var fileReader = new StreamReader("../../../../test.in"))
+        {
+            var parser = new global::SimpleParser.SimpleParser(fileReader);
+            try
+            {
+                parser.ParseExpression().PrintTree("");
+                //Console.Write(parser.ParseExpression().Calc());
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+    }
+}
